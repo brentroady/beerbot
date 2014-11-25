5c06040948ab936cece4520573931fa4eaa999af
Brent Roady <broady@ffncorp.com>
2014-11-25 16:45:42 -0600

    init part 2

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-﻿# NodejsApp1
+﻿# @NewBeerCreator (beerbot)
 
+A Twitter bot that creates new beer ideas
 
+## Description
+
+Uses [NodeJS](http://nodejs.org/), the [twit](https://github.com/ttezel/twit) library and the [BreweryDB](http://www.brewerydb.com/) API to build new beer ideas. 
+
+## License
+Copyright (c) 2014 Brent Roady
+Licensed under the MIT license.
--- server.js
+++ server.js
@@ -1,41 +1,67 @@
-﻿/*
-var restify = require('restify');
-
-function respond (req, res, next) {
-    res.send('hello ' + req.params.name);
-    next();
-}
-
-var server = restify.createServer();
-
-server.get('/hello/:name', respond);
-
-*/
-var express = require('express');
+﻿var express = require('express');
 var app = express();
 
 var Twit = require('twit');
 var T = new Twit(require('./config.js'));
-var restclient = require('node-restclient');
-var markov = require('markov');
-var m = markov(1);
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function (request, response) {
-    response.send(m.fill(m.pick(), 2).join(' '));
-})
 
-var beers = {};
-restclient.get('http://api.brewerydb.com/v2/beers/?key=9149d3bb6685a717143de335cbc68fcc&styleId=3', function(res) { beers = JSON.parse(res); seedMarkov() });
-function seedMarkov() {
-    var names = '';
-    beers.data.forEach(function (d) {
-        names += d.name + ' ';
-    });
-    m.seed(names);
+var styles = require('./styles');
+var adjuncts = require('./adjuncts');
+var hops = require('./hops');
+
+function CreateSomethingNew() {
+
+    var MAX_LENGTH = 140;
+    var len = 141;
+    var something = '';
+
+    while (len > MAX_LENGTH) {
+
+        var style = styles[Math.floor(Math.random() * styles.length)].name;
+        var adjunct = adjuncts[Math.floor(Math.random() * adjuncts.length)].name;
+        var hop = hops[Math.floor(Math.random() * hops.length)].name;
+
+        something = style + ' with ' + adjunct + ' and ' + hop + ' hops';
+        len = something.length;
+
+        console.log(something + '; length: ' + len);
+    }
+
+    return something;
 }
+
+function tweet() {
+
+    var newbeer = CreateSomethingNew();
+
+    T.post('statuses/update', { status: newbeer }, function(err, reply) {
+        if (err) {
+            console.log('error:', err);
+        }
+        else {
+            console.log('reply:', reply);
+        }
+    });
+}
+
+tweet();
+
+setInterval(function() {
+    try {
+        tweet();
+    }
+    catch (e) {
+        console.error(e);
+    }
+
+}, 1000 * 60 * 60);
+
+app.get('/', function (request, response) {
+    response.send(CreateSomethingNew());
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
